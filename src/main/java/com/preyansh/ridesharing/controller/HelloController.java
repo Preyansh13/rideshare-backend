7625f39f102110cd7f974a471d82adb0537d2f72
Preyansh Sharma <preyansh302@gmail.com>
2026-09-11 23:27:11 +0530

    Day 2: Learn request mappings and parameters

--- src/main/java/com/preyansh/ridesharing/controller/HelloController.java
+++ src/main/java/com/preyansh/ridesharing/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
@@ -12,4 +15,17 @@ public class HelloController {
     public String hello() {
         return "Ride Sharing Backend is running!";
     }
+
+//    For practice purpose of @PathVariable and @RequestParam annotations
+
+//    @GetMapping("/hello/{name}")
+//    public String helloName(@PathVariable String name) {
+//        return "Hello " + name + "! This is @PathVariable trial.";
+//    }
+//
+//    @GetMapping("/greet")
+//    public String greet(@RequestParam String name) {
+//        return "Hello " + name + "! This is @RequestParam trial.";
+//    }
+
 }
