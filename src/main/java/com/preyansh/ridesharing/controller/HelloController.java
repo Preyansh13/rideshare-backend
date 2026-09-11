130a6b0966acb5be92249a4ddc59674c956e503f
Preyansh Sharma <preyansh302@gmail.com>
2026-09-11 18:59:26 +0530

    Day 1: Initialize RideShare backend

---
+++ src/main/java/com/preyansh/ridesharing/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.preyansh.ridesharing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Ride Sharing Backend is running!";
+    }
+}
